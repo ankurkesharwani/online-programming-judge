e357d8607069f2eb9f2b34345cfca4f90970bcd1
kesharwani_ankur <ankur.kesharwani001@gmail.com>
2015-04-07 00:48:54 +0530

    Removed unused code.

--- bazinga/dbinfo/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$db_hostname = 'localhost';
-$db_database='BAZINGA';
-$db_usrname = 'BAZINGA';
-$db_passwd = 'BAZINGA';
-$db_server = mysql_connect($db_hostname, $db_usrname, $db_passwd);
-if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
-mysql_select_db($db_database)or die("Unable to select database: " . mysql_error());
-?>
